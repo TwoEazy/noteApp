a58cb16d5988d524eb8c8e8462016e092b4f8053
moaaz.eljamous <moaaz.eljamous@studet.howest.be>
2025-05-29 19:55:06 +0200

    reverted accidental delete of note class

--- app/src/main/java/com/noteapp/data/model/Note.kt
+++ app/src/main/java/com/noteapp/data/model/Note.kt
@@ -1,4 +1,9 @@
 package com.noteapp.data.model
 
-class Note {
-}
+data class Note(
+    val id: Int = 0,
+    val title: String,
+    val content: String,
+    val createdAt: Long,
+    val updatedAt: Long
+)
