8253124ae86d05c5d30277b64d339523d3985052
moaaz.eljamous <moaaz.eljamous@studet.howest.be>
2025-05-29 19:43:04 +0200

    fixing imports

--- app/src/main/java/com/noteapp/MainActivity.kt
+++ app/src/main/java/com/noteapp/MainActivity.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.noteapp.di.AppContainer
 import com.noteapp.navigation.Navigation
